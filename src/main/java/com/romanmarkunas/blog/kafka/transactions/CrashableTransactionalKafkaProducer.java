7a6bf345ee19a6807946be168fee1f5968ec45fe
Romans Markuns <romans.markuns@ig.com>
2018-10-06 23:16:26 +0100

    crashable transactional producer draft

--- src/main/java/com/romanmarkunas/blog/kafka/transactions/CrashableTransactionalKafkaProducer.java
+++ src/main/java/com/romanmarkunas/blog/kafka/transactions/CrashableTransactionalKafkaProducer.java
@@ -1,12 +1,41 @@
 package com.romanmarkunas.blog.kafka.transactions;
 
 import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class CrashableTransactionalKafkaProducer {
 
-   private final KafkaProducer<Integer, String> kafkaProducer;
-   private boolean crashDuringTransaction;
+   // TODO - describe caveats with initTransactions + replication
+   private final KafkaProducer<Integer, String> producer;
+
+   private AtomicInteger transactionCount = new AtomicInteger(0);
+   private boolean shouldCrash = false;
 
 
+   public CrashableTransactionalKafkaProducer(KafkaProducer<Integer, String> producer) {
+      this.producer = producer;
+   }
 
+
+   public void sendTransactionally() {
+      int transactionNr = transactionCount.incrementAndGet();
+
+      synchronized (producer) {
+         producer.beginTransaction();
+
+         producer.send(new ProducerRecord<>("topic1", transactionNr, "msg1"));
+         if (shouldCrash) {
+            throw new RuntimeException("Crash in the middle of transaction!");
+         }
+         producer.send(new ProducerRecord<>("topic2", transactionNr, "msg2"));
+
+         producer.commitTransaction();
+      }
+   }
+
+   public void setShouldCrash(boolean shouldCrash) {
+      this.shouldCrash = shouldCrash;
+   }
 }
